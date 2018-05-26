b802b7332a94ac609b20b2ae2b550bd9b9ee26d9
Dmitry Ponyatov <ponyatov@toshka.local>
2018-05-26 20:29:21 +0400

    Signed-off-by: Dmitry Ponyatov <ponyatov@toshka.local>

--- Q.py
+++ Q.py
@@ -1,3 +1,37 @@
+MODULE  = 'Q'
+TITLE   = 'Qbject system'
+GITHUB  = 'https://github.com/ponyatov/Q'
+AUTHOR  = 'Dmitry Ponyatov'
+EMAIL   = 'dponyatov@gmail.com'
+LICENSE = 'All rights reserved'
+
+README  = '''
+# %s
+## %s
+
+(c) %s <<%s>>, %s
+
+github: %s
+'''%(MODULE,TITLE,AUTHOR,EMAIL,LICENSE,GITHUB)
+
 import sys
 
-print sys.argv
+# use wxPython
+import wx
+# and Scintilla editor
+import wx.stc
+
+# wxApplication
+app = wx.App()
+
+# main window
+main = wx.Frame(None,title='%s'%sys.argv) ; main.Show()
+
+# menu
+menu = wx.MenuBar() ; main.SetMenuBar(menu)
+
+# editor
+editor = wx.stc.StyledTextCtrl(main) ; editor.SetValue(README)
+
+# start GUI
+app.MainLoop()
