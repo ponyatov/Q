4a9f66890aab2f3124bf109dcf16dbae4e2dfd4f
Dmitry Ponyatov <dponyatov@gmail.com>
2018-05-28 22:18:58 +0400

    + pygame sample
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ Game/game.py
@@ -0,0 +1,58 @@
+import pygame
+pygame.display.init()
+
+pygame.display.set_mode((640,480)) 
+
+raw_input()
+
+# import os,sys
+#  
+# # size = [320,240]
+# # 
+# # # print pygame.display.Info()
+# # # 
+# # # print pygame.display.set_mode(size)
+# # # 
+# # # raw_input()
+#  
+# import wx
+#  
+# app = wx.App()
+#  
+# form = wx.Frame(None) ; form.Show()
+#  
+# # menu = wx.MenuBar() ; form.SetMenuBar(menu)
+# # 
+# # file = wx.Menu() ; menu.Append(file,'&File')
+# # quit = file.Append(wx.ID_EXIT,'&Quit')
+# # form.Bind(wx.EVT_MENU,lambda e:form.Close(),quit)
+# 
+# # panel = wx.Panel(form)
+#  
+# hwnd = form.GetHandle() ; print hwnd
+#  
+# os.environ['SDL_WINDOWID'] = str(hwnd)
+#  
+# # avi = pygame.movie.Movie('drop.avi')
+# # if avi.has_video(): w,h=avi.get_size()
+# # 
+# # display = pygame.display.set_mode((w,h))
+# # # display = pygame.display.set_mode((w,h))
+# # 
+# # avi.set_display(display)
+# # avi.play()
+# # 
+# # # app.MainLoop()
+# 
+# import pygame
+# pygame.display.init()
+#   
+# # avi = pygame.movie.Movie('drop.avi') ; print avi
+# # if avi.has_video(): w,h=avi.get_size() ; print w,h
+#  
+# display = pygame.display.set_mode(form.GetSizeTuple())
+#   
+# # avi.set_display(display)
+# # avi.play()
+# 
+# raw_input()
