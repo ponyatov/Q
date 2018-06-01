59476dc0f1e813eee56b21d01c6a47195529d8ae
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-06-01 09:10:19 +0400

    * CMSIS subproject
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- Game/game.py
+++ Game/game.py
@@ -1,85 +1,46 @@
-# import os
-# 
-# import wx
-# 
-# app = wx.App()
-# 
-# form = wx.Frame(None) ; form.Show()
-# 
-# win = wx.Window(form) ; form.display = win
-# os.environ['SDL_WINDOWID'] = str(win.GetHandle())
-
-import random,math
- 
+import sys  
 import pygame
-pygame.display.init()
- 
-scr = pygame.display.set_mode((640,480))
-print scr
+pygame.init()
+pygame.display.set_caption(str(sys.argv))
 
-r = g = b = 0
+scr = pygame.display.set_mode((240,320))
 
 while True:
-    r0 = r ; g0 = g ; b0 = b 
-    r = random.randint(0x00,0xFF)
-    g = random.randint(0x00,0xFF)
-    b = random.randint(0x00,0xFF)
-    dr = (r-r0)/100
-    dg = (g-g0)/100
-    db = (b-b0)/100
-    for i in range(100):
-        r += dr ; g += dg ; b += db
-        scr.fill((r,g,b))
-        pygame.display.update()
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT: break
 
-raw_input()
- 
- 
-# def onPaint(e):
-#     scr.fill((0,0,0))
-#     pygame.display.update()
-# win.Bind(wx.EVT_PAINT, onPaint)
-#  
-# app.MainLoop()
+# # win = wx.Window(form) ; form.display = win
+# # os.environ['SDL_WINDOWID'] = str(win.GetHandle())
 # 
-# # import os,sys
+# # def onPaint(e):
+# #     scr.fill((0,0,0))
+# #     pygame.display.update()
+# # win.Bind(wx.EVT_PAINT, onPaint)
 # #  
-# # # size = [320,240]
-# # # 
-# # # # print pygame.display.Info()
-# # # # 
-# # # # print pygame.display.set_mode(size)
-# # # # 
-# # # # raw_input()
-# #  
-# # import wx
-# #  
-# # app = wx.App()
-# #  
-# # form = wx.Frame(None) ; form.Show()
-# #  
-# # # menu = wx.MenuBar() ; form.SetMenuBar(menu)
-# # # 
-# # # file = wx.Menu() ; menu.Append(file,'&File')
-# # # quit = file.Append(wx.ID_EXIT,'&Quit')
-# # # form.Bind(wx.EVT_MENU,lambda e:form.Close(),quit)
+# # print pygame.display.Info()
 # # 
-# # # panel = wx.Panel(form)
-# #  
-# # hwnd = form.GetHandle() ; print hwnd
-# #  
-# # os.environ['SDL_WINDOWID'] = str(hwnd)
-# #  
-# # # avi = pygame.movie.Movie('drop.avi')
-# # # if avi.has_video(): w,h=avi.get_size()
+# # print pygame.display.set_mode(size)
+# # 
+# # menu = wx.MenuBar() ; form.SetMenuBar(menu)
+# # 
+# # file = wx.Menu() ; menu.Append(file,'&File')
+# # quit = file.Append(wx.ID_EXIT,'&Quit')
+# # form.Bind(wx.EVT_MENU,lambda e:form.Close(),quit)
 # # # 
+# # # # panel = wx.Panel(form)
+# # #  
+# # # hwnd = form.GetHandle() ; print hwnd
+# # #  
+# # # os.environ['SDL_WINDOWID'] = str(hwnd)
+# # #  
+# # avi = pygame.movie.Movie('drop.avi')
+# # if avi.has_video(): w,h=avi.get_size()
+# # 
+# # display = pygame.display.set_mode((w,h))
 # # # display = pygame.display.set_mode((w,h))
-# # # # display = pygame.display.set_mode((w,h))
-# # # 
-# # # avi.set_display(display)
-# # # avi.play()
-# # # 
-# # # # app.MainLoop()
+# # 
+# # avi.set_display(display)
+# # avi.play()
 # # 
 # # import pygame
 # # pygame.display.init()
@@ -91,5 +52,3 @@ raw_input()
 # #   
 # # # avi.set_display(display)
 # # # avi.play()
-# # 
-# # raw_input()
