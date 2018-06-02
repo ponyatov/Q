b5b15a7fbffece3917577d14aa84fbd2a9d3c999
Dmitry Ponyatov <ponyatov@toshka.local>
2018-06-02 13:37:35 +0400

    + img/bg
    Signed-off-by: Dmitry Ponyatov <ponyatov@toshka.local>

--- Game/coco.py
+++ Game/coco.py
@@ -3,5 +3,33 @@
 
 # install: $ sudo pip install cocos2d
 
+import sys
 import cocos
+from cocos.director import director
+from cocos.layer import Layer
+from cocos.scene import Scene
+from cocos.sprite import Sprite
 
+class IntroLayer(Layer):
+    def __init__(self):
+		Layer.__init__(self)
+		label = cocos.text.Label("Hell Cocos", font_size=H / 2 / 5,
+			anchor_x='center', anchor_y='top')
+		label.position = W / 2, H-5
+		self.add(label)
+        
+class TetrisBG(Sprite):
+    def __init__(self):
+        Sprite.__init__(self,'img/tetris/bg.png',scale=.4)
+        self.image_anchor_y = 0
+        self.position = W/2,0
+
+director.init(caption=str(sys.argv))
+W, H = director.get_window_size()
+
+# bg_l = TetrisBG()
+intro_l = IntroLayer()
+
+intro_s = Scene(TetrisBG()) ; intro_s.add(intro_l)
+
+director.run(intro_s)
