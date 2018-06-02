f4d9449d0a4b2f34cb33fd27f1f27bfc29b96ce5
Dmitry Ponyatov <ponyatov@toshka.local>
2018-06-02 14:19:21 +0400

    + sprite animation
    Signed-off-by: Dmitry Ponyatov <ponyatov@toshka.local>

--- Game/coco.py
+++ Game/coco.py
@@ -9,6 +9,8 @@ from cocos.director import director
 from cocos.layer import Layer
 from cocos.scene import Scene
 from cocos.sprite import Sprite
+import pyglet
+from _bsddb import DB_GID_SIZE
 
 class IntroLayer(Layer):
     def __init__(self):
@@ -23,13 +25,24 @@ class TetrisBG(Sprite):
         Sprite.__init__(self,'img/tetris/bg.png',scale=.4)
         self.image_anchor_y = 0
         self.position = W/2,0
+        
+class Ani(Layer):
+    def __init__(self):
+        Layer.__init__(self)
+        sheet = pyglet.image.load('img/tetris/walk.png')
+        grid = pyglet.image.ImageGrid(sheet,2,8)[0:]
+        anim = pyglet.image.Animation.from_image_sequence(grid, .1, loop=True)
+        spr = Sprite(anim) ; spr.position = W/2,H/2
+        self.add(spr)
 
 director.init(caption=str(sys.argv))
 W, H = director.get_window_size()
 
-# bg_l = TetrisBG()
 intro_l = IntroLayer()
 
-intro_s = Scene(TetrisBG()) ; intro_s.add(intro_l)
+intro_s = Scene()
+intro_s.add(TetrisBG(),0)
+intro_s.add(intro_l)
+intro_s.add(Ani(),9)
 
 director.run(intro_s)
